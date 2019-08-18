3455ac89c36efef3e34c63a4602908c0342f3e5e
Ruannilton <44852912+Ruannilton@users.noreply.github.com>
2019-08-18 17:34:54 -0300

    New Mesh and MeshLoader

--- Src/Engine/Graphic/Mesh.c
+++ Src/Engine/Graphic/Mesh.c
@@ -11,37 +11,23 @@ GLuint mesh_genVAO(Mesh* mesh) {
 	glGenVertexArrays(1, &vao);
 	glBindVertexArray(vao);
 
+	glGenBuffers(1, &vbo);
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);
+	
+	uint v_size = mesh->vertices_count*sizeof(uint);
+	uint n_size = mesh->normal_count*sizeof(uint);
+	uint t_size = mesh->uv_count*sizeof(uint);
+	uint c_size = mesh->color_count*sizeof(uint);
+
+	glBufferSubData(GL_ARRAY_BUFFER, 0,v_size, mesh->vertices);
+	glBufferSubData(GL_ARRAY_BUFFER, v_size, n_size, mesh->normals);
+	glBufferSubData(GL_ARRAY_BUFFER, v_size + n_size, t_size, mesh->uvs);
+	glBufferSubData(GL_ARRAY_BUFFER, v_size + n_size + t_size, c_size, mesh->colors);
 
 	glGenBuffers(1, &ebo);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ebo);
-	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->indexCount * sizeof(unsigned int), mesh->index, GL_STATIC_DRAW);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, mesh->index_count * sizeof(unsigned int), mesh->index, GL_STATIC_DRAW);
 
-	glGenBuffers(1, &vbo);
-	glBindBuffer(GL_ARRAY_BUFFER, vbo);
-	glBufferData(GL_ARRAY_BUFFER, mesh->vertexCount * sizeof(float), mesh->vertex, GL_STATIC_DRAW);
-
-
-
-	if (mesh->flag & POSITION_FLAG) {
-		glEnableVertexAttribArray(0);
-		glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, mesh->vertexSize * sizeof(float), (void*)(count * sizeof(float)));
-		count += 3;
-	}
-	if (mesh->flag & COLOR_FLAG) {
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(1, 3, GL_FLOAT, GL_FALSE, mesh->vertexSize * sizeof(float), (void*)(count * sizeof(float)));
-		count += 3;
-	}
-	if (mesh->flag & NORMAL_FLAG) {
-		glEnableVertexAttribArray(1);
-		glVertexAttribPointer(3, 3, GL_FLOAT, GL_FALSE, mesh->vertexSize * sizeof(float), (void*)(count * sizeof(float)));
-		count += 3;
-	}
-	if (mesh->flag & UV_FLAG) {
-		glEnableVertexAttribArray(2);
-		glVertexAttribPointer(2, 2, GL_FLOAT, GL_FALSE, mesh->vertexSize * sizeof(float), (void*)(count * sizeof(float)));
-		count += 2;
-	}
 
 	glBindVertexArray(0);
 	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, 0);
@@ -59,7 +45,6 @@ Mesh* mesh_LoadMesh(string path) {
 	unsigned int index_count = 0;
 	unsigned int norm_count = 0;
 	unsigned int uv_count = 0;
-	unsigned int vertSize = 3;
 	unsigned int i;
 
 	vec3* vertList = (vec3*)malloc(sizeof(vec3*) * (mesh_loader_pre_alloc + 1));
@@ -68,9 +53,6 @@ Mesh* mesh_LoadMesh(string path) {
 	vec3* faceList = (vec3*)malloc(sizeof(vec3*) * (9 * (mesh_loader_pre_alloc + 1)));
 
 	Mesh* mesh = (Mesh*)malloc(sizeof(Mesh));
-
-	float* mesh_verts;
-	unsigned int* mesh_index;
 	int index_perline = 0;
 
 #pragma region READ FILE
@@ -79,15 +61,15 @@ Mesh* mesh_LoadMesh(string path) {
 		if (fscanf_s(file, "%s", &desc)) {
 
 			if (strcmp(desc, "v") == 0) {
-				vert_count += 1;
+				vert_count += 3;
 				fscanf_s(file, "%f %f %f", &vertList[vert_count][0], &vertList[vert_count][1], &vertList[vert_count][2]);
 			}
 			else if (strcmp(desc, "vn") == 0) {
-				norm_count += 1;
+				norm_count += 3;
 				fscanf_s(file, "%f %f %f", &normList[norm_count][0], &normList[norm_count][1], &normList[norm_count][2]);
 			}
 			else if (strcmp(desc, "vt") == 0) {
-				uv_count += 1;
+				uv_count += 2;
 				fscanf_s(file, "%f %f", &uvList[uv_count][0], &uvList[uv_count][1]);
 			}
 			else if (strcmp(desc, "f") == 0) {
@@ -131,30 +113,25 @@ Mesh* mesh_LoadMesh(string path) {
 	}
 #pragma endregion
 
-	mesh->flag = POSITION_FLAG;
+	mesh->normal_count = norm_count;
+	mesh->vertices_count = vert_count;
+	mesh->uv_count = uv_count;
+	mesh->index_count = index_count;
 
-	if (uv_count != 0) {
-		mesh->flag |= UV_FLAG;
-		vertSize += 2;
-	}
-	if (norm_count != 0) {
-		mesh->flag |= NORMAL_FLAG;
-		vertSize += 3;
-	}
-
-	mesh_verts = (float*)malloc(sizeof(float) * (vertSize * index_count));
-	mesh_index = (unsigned int*)malloc(sizeof(unsigned int) * index_count);
-	mesh->vertexCount = vertSize * index_count;
-	mesh->indexCount = index_count;
-	mesh->vertexSize = vertSize;
+	mesh->vertices = (float*)malloc(sizeof(float) *3*index_count);
+	mesh->normals  = (float*)malloc(sizeof(float) *3*index_count);
+	mesh->index    = (uint*) malloc(sizeof(uint)  *1*index_count);
+	mesh->uvs 	   = (float*)malloc(sizeof(float) *2*index_count);
+	
 
 	unsigned int verti = 0;
+	unsigned int vertu = 0;
 	int iv, in, it;
 	float v1, v2, v3, t1, t2, n1, n2, n3;
 
 	for (i = 0; i < index_count; i++) {
 
-		mesh_index[i] = i;
+		mesh->index[i] = i;
 
 		iv = (int)faceList[i][0];
 		it = (int)faceList[i][1];
@@ -170,86 +147,60 @@ Mesh* mesh_LoadMesh(string path) {
 		n3 = normList[in][2];
 
 		if (uv_count && norm_count) {
-			mesh_verts[verti] = v1;
-			mesh_verts[verti + 1] = v2;
-			mesh_verts[verti + 2] = v3;
+			mesh->vertices[verti] = v1;
+			mesh->vertices[verti + 1] = v2;
+			mesh->vertices[verti + 2] = v3;
 
-			mesh_verts[verti + 3] = n1;
-			mesh_verts[verti + 4] = n2;
-			mesh_verts[verti + 5] = n3;
+			mesh->normals[verti ] = n1;
+			mesh->normals[verti + 1] = n2;
+			mesh->normals[verti + 2] = n3;
 
-			mesh_verts[verti + 6] = t1;
-			mesh_verts[verti + 7] = t2;
+			mesh->uvs[vertu] = t1;
+			mesh->uvs[vertu+1] = t2;
 
+			verti += 3;
+			vertu += 2;
 		}
 		else if (uv_count) {
-			mesh_verts[verti] = v1;
-			mesh_verts[verti + 1] = v2;
-			mesh_verts[verti + 2] = v3;
+			mesh->vertices[verti] = v1;
+			mesh->vertices[verti + 1] = v2;
+			mesh->vertices[verti + 2] = v3;
 
-			mesh_verts[verti + 3] = t1;
-			mesh_verts[verti + 4] = t2;
+			mesh->uvs[vertu] = t1;
+			mesh->uvs[vertu+1] = t2;
+
+			verti += 3;
+			vertu += 2;
 
 		}
 		else if (norm_count) {
-			mesh_verts[verti] = v1;
-			mesh_verts[verti + 1] = v2;
-			mesh_verts[verti + 2] = v3;
+			mesh->vertices[verti] = v1;
+			mesh->vertices[verti + 1] = v2;
+			mesh->vertices[verti + 2] = v3;
 
-			mesh_verts[verti + 3] = n1;
-			mesh_verts[verti + 4] = n2;
-			mesh_verts[verti + 5] = n3;
-
+			mesh->normals[verti ] = n1;
+			mesh->normals[verti + 1] = n2;
+			mesh->normals[verti + 2] = n3;
+			
+			verti += 3;
 		}
 
-		verti += vertSize;
 	}
-
-	mesh->vertex = mesh_verts;
-	mesh->index = mesh_index;
 
 	free(vertList);
 	free(normList);
 	free(uvList);
 	free(faceList);
+
 	fclose(file);
 	mesh_genVAO(mesh);
 	printf("Loaded %s\n", path);
 	return mesh;
 }
 
-Mesh* mesh_BuildMesh(unsigned int* index, float* vertex, unsigned int flags, int indexCount, int vertexCount) {
-	Mesh* mesh = NEW(Mesh);
-	mesh->index = index;
-	mesh->vertex = vertex;
-	mesh->flag = flags;
-	mesh->indexCount = indexCount;
-	mesh->vertexCount = vertexCount;
-	mesh_calculateVertexSize(mesh);
-	return mesh;
-}
-
-void mesh_calculateVertexSize(Mesh* mesh) {
-	mesh->vertexSize = 0;
-	if (mesh->flag & POSITION_FLAG) mesh->vertexSize += 3;
-	if (mesh->flag & COLOR_FLAG) mesh->vertexSize += 3;
-	if (mesh->flag & NORMAL_FLAG) mesh->vertexSize += 3;
-	if (mesh->flag & UV_FLAG) mesh->vertexSize += 2;
-}
-
 void mesh_PrintMesh(Mesh* mesh) {
-	unsigned int i = 0;
-
-	printf("Index Count: %d\n", mesh->indexCount);
-	printf("Vertex Count: %d\n", mesh->vertexCount);
-	printf("Vertex Size: %d\n", mesh->vertexSize);
-
-	for (i = 0; i < mesh->indexCount; i++) {
-		printf("Index: %d\n", mesh->index[i]);
-	}
-	for (i = 0; i < mesh->vertexCount; i += mesh->vertexSize) {
-		printf("Vertex: %f,%f,%f  %f,%f,%f  %f,%f\n", mesh->vertex[i], mesh->vertex[i + 1], mesh->vertex[i + 2], mesh->vertex[i + 3], mesh->vertex[i + 4], mesh->vertex[i + 5], mesh->vertex[i + 6], mesh->vertex[i + 7]);
-	}
+	printf("Index Count: %d\n", mesh->index_count);
+	printf("Vertex Count: %d\n", mesh->vertices_count);
 }
 
 
--- Src/Engine/Graphic/Mesh.h
+++ Src/Engine/Graphic/Mesh.h
@@ -13,30 +13,27 @@
 UNIQUE int mesh_loader_pre_alloc = kbyte(0.5);
 
 typedef struct {
-	float* vertex;
-	unsigned int* index;
-	GLuint mesh_id;
-	unsigned int flag;
-	unsigned int vertexSize;
-	unsigned int vertexCount;
-	unsigned int indexCount;
+	float* vertices;
+	uint* index;
+	float* uvs;
+	float* colors;
+	float* normals;
+
+	uint vertices_count;
+	uint index_count;
+	uint uv_count;
+	uint color_count;
+	uint normal_count;
+	uint mesh_id;
 }Mesh;
 
 
-GLuint mesh_genVAO(Mesh* mesh);
 
+GLuint mesh_genVAO(Mesh* mesh);
 Mesh* mesh_LoadMesh(string path);
-Mesh* mesh_BuildMesh(unsigned int* index, float* vertex, unsigned int flags, int indexCount, int vertexCount);
 
 void mesh_PrintMesh(Mesh* mesh);
 void mesh_calculateVertexSize(Mesh* mesh);
 
-
-__inline void mesh_EnableFlags(Mesh* m, unsigned int flags) {
-	m->flag |= flags;
-}
-__inline void mesh_DisableFlags(Mesh* m, unsigned int flags) {
-	m->flag ^= flags;
-}
 #endif // !NANO_MESH
 
