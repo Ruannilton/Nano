7bd162eb37922b1281f110194b6d9f4fb944284c
Ruannilton <44852912+Ruannilton@users.noreply.github.com>
2019-08-24 18:44:07 -0300

    Data Structures Ini

---
+++ Src/Engine/Data Structures/LinkedTable.h
@@ -0,0 +1,9 @@
+#ifndef NANO_LINKED_TABLE
+#define NANO_LINKED_TABLE
+
+#include "NanoDataStructures.h"
+
+
+
+
+#endif // !NANO_LINKED_TABLE
---
+++ Src/Engine/Data Structures/NanoDataStructures.h
@@ -0,0 +1,6 @@
+#ifndef NANO_DATA_STRUCTURES
+#define NANO_DATA_STRUCTURES
+
+#include "..//Utils.h"
+
+#endif // !NANO_DATA_STRUCTURES
