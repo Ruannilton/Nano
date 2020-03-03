99202f9f2d3de31f2a271977ef7516c24fd61b4e
Ruannilton <44852912+Ruannilton@users.noreply.github.com>
2020-03-03 00:34:33 -0300

    Shader include precompiler

--- Assets/Shaders/default.vert
+++ Assets/Shaders/default.vert
@@ -1,30 +1,5 @@
-#version 430 core
+#include "NanoEngine.vert.glh"
 
-layout(std140, binding = 0) uniform Matrices
-{
-	mat4 projection;
-	mat4 view;
-	mat4 lightMatrix;
-};
+void NanoMain(){
 
-layout (location = 0) in vec3 aPos;
-layout (location = 1) in vec4 aColor;
-layout (location = 2) in vec2 aTexCoord;
-layout (location = 3) in vec3 aNormal;
-layout (location = 4) in mat4 aInstancedModel;
-
-out vec4 ourColor;
-out vec2 TexCoord;
-out vec3 FragPos;
-out vec3 Normal;
-out vec4 FragLightPos;
-
-void main()
-{
-    gl_Position =projection*view*aInstancedModel*vec4(aPos, 1.0);
-    TexCoord = aTexCoord;
-	ourColor = aColor;
-	FragPos = vec3(aInstancedModel*vec4(aPos, 1.0));
-	Normal = mat3(transpose(inverse(aInstancedModel))) * aNormal;
-	FragLightPos = lightMatrix*vec4(FragPos,1.0);
 }
--- Src/Engine/Graphic/Shader.c
+++ Src/Engine/Graphic/Shader.c
@@ -21,6 +21,70 @@ unsigned int Shader_CompileShader(string path,string src, GLenum type) {
 	return id;
 }
 
+string PreCompiler(string str) {
+	
+	char file_name[64];
+	char* loc_finded = strstr(str, "#include");
+	char* new_ptr;
+	char* ret = 0;
+	if (loc_finded == NULL) return str;
+	
+	while (loc_finded != NULL) {
+		char* ini = loc_finded + 8;
+		int readed = 0;
+		int ini_read_name = 0;
+		int i = 0;
+
+		while (true)
+		{
+			char v = ini[i];
+			i++;
+			if (v == ' ') continue;
+			if (v == '\"' && ini_read_name == 0) {
+				ini_read_name = 1;
+				continue;
+			}
+
+			if (v == '\"' && ini_read_name) break;
+			file_name[readed] = v;
+			readed++;
+
+		}
+		char* end = ini + i;
+		file_name[readed] = '\0';
+		char* path = malloc(128);
+		if (path) {
+			strcpy(path, "Assets/Shaders/");
+			path = strcat(path, file_name);
+		}
+		string module = IO_ReadFile(path);
+		size_t  src_size = strlen(str);
+		size_t  module_size = strlen(module);
+
+		char* aux = malloc(src_size);
+		if (aux) aux = strcpy(aux, end);
+
+
+		 new_ptr = malloc(src_size + module_size + 4);
+
+		if (new_ptr) {
+			strcpy(new_ptr, str);
+			loc_finded = strstr(new_ptr, "#include");
+			strcpy(loc_finded, module);
+			strcat(new_ptr, aux);
+		}
+
+		loc_finded = strstr(new_ptr, "#include");
+		ret = new_ptr;
+		free(aux);
+		free(path);
+		free(str);
+	}
+	
+	
+	return ret;
+}
+
 Shader Shader_CreateShader(string vs, string fs) {
 
 	
@@ -28,14 +92,24 @@ Shader Shader_CreateShader(string vs, string fs) {
 	unsigned int vsID = 0;
 	unsigned int fsID = 0;
 
-	vsID = Shader_CompileShader(vs,IO_ReadFile(vs), GL_VERTEX_SHADER);
-	fsID = Shader_CompileShader(fs,IO_ReadFile(fs), GL_FRAGMENT_SHADER);
+	string vs_text = IO_ReadFile(vs);
+	string fs_Text = IO_ReadFile(fs);
+
+	vs_text = PreCompiler(vs_text);
+	fs_Text = PreCompiler(fs_Text);
+	
+	vsID = Shader_CompileShader(vs, vs_text, GL_VERTEX_SHADER);
+	fsID = Shader_CompileShader(fs, fs_Text, GL_FRAGMENT_SHADER);
 
 	glAttachShader(prog, vsID);
+	
 	glAttachShader(prog, fsID);
+	
+	
 	glLinkProgram(prog);
+	
 	glValidateProgram(prog);
-
+	
 	glDeleteShader(vsID);
 	glDeleteShader(fsID);
 	
@@ -43,11 +117,14 @@ Shader Shader_CreateShader(string vs, string fs) {
 	
 	if (err != GL_NO_ERROR)
 	{
-		//GLchar* str = glErrorStringREGAL(err);
-		//DEBUG_C(ANSI_RED, "%s", str);
+		GLchar* str = glErrorStringREGAL(err);
+		DEBUG_C(ANSI_RED, "%s", str);
 	}
 	else
 		DEBUG_C(ANSI_LIGHT_GREEN, "SHADER(%d) LOADED", prog);
 	return prog;
+
+	free(vs_text);
+	free(fs_Text);
 }
 
